14f2fd8920b9f8b8a6c85fbd5171511c6b44ab56
Tucker Wash <tucker@songbird.sh>
2021-12-19 12:23:49 -0500

    adds description to node class

--- node.rb
+++ node.rb
@@ -1,4 +1,15 @@
 module Raft
   class Node
+    # Raft provides the following guarantees
+    # Election Safety:
+    #   at most one leader can be elected in a given term.
+    # Leader Append-Only:
+    #   a leader never overwrites or deletes entries in its log; it only appends new entries.
+    # Log Matching:
+    #   if two logs contain an entry with the same index and term, then the logs are identical in all entries up through the given index.
+    # Leader Completeness:
+    #   if a log entry is committed in a given term, then that entry will be present in the logs of the leaders for all higher-numbered terms.
+    # State Machine Safety:
+    #   if a server has applied a log entry at a given index to its state machine, no other server will ever apply a different log entry for the same index.
   end
 end
