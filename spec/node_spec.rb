2431f6f59185b501567717c5f9e32574a0323680
Tucker Wash <tucker@songbird.sh>
2021-12-19 16:50:26 -0500

    it should initialize with a heartbeat timeout between 150ms and 300ms

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -7,5 +7,11 @@ describe Raft::Node do
     it 'should be in a follower state' do
       expect(node.type).to eq :FOLLOWER
     end
+
+    it ' initialize with a heartbeat timeout between 150ms and 300ms' do
+      expect(node.heartbeat_timeout.class).to be Integer
+      expect(node.heartbeat_timeout).to be >= 150
+      expect(node.heartbeat_timeout).to be <= 300
+    end
   end
 end
